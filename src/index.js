02068fe3afdb449437f45319e92bb98f69feab79
Hubert Marzecki <hubert.marzecki@gmail.com>
2020-04-28 12:52:31 +0200

    n

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import scroll from './scripts/scroll'
 import glider from './scripts/glider'
 import currentYear from './scripts/currentYear'
 import allProducts from './scripts/productDisplay'
-
+import products from './scripts/products'
 
 window.addEventListener('DOMContentLoaded', (event) => {
     scroll();
     glider();
     currentYear();
     allProducts();
+    products();
 
 });
 
